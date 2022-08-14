2c8da30b5951ae068b4a7e6583919a122312b4fc
jaykumar <jaykumardave.06@gmail.com>
2022-08-14 16:42:22 +1000

    initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Footer = () => {
+  
+  return (
+
+   <footer className="bg-dark text-light py-4">
+    <p className="text-center">
+      Copyright &copy; MyTodoslist.com 
+    </p>
+     
+  </footer>
+   
+  )
+}
